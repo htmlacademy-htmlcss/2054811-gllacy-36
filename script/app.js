240dce0c4aeee86ba4c9e54149e1f65a8b904643
julia_celarscaia <julia.celarscaia@gmail.com>
2023-03-02 22:16:03 -0800

    cart-block srction was added

--- script/app.js
+++ script/app.js
@@ -2,4 +2,14 @@ document.querySelector('.navigation-link-select').addEventListener('click', func
   e.preventDefault();//no reloading page
   document.querySelector('.navigation-item-select').classList.toggle('navigation-item-select--open');
 
+});
+document.querySelector('.navigation-user-link-search').addEventListener('click', function (e) {
+  // e.preventDefault();//no reloading page
+  document.querySelector('.navigation-item-search').classList.toggle('navigation-item-search--open');
+
+});
+document.querySelector('.navigation-item-cart').addEventListener('click', function (e) {
+  // e.preventDefault();//no reloading page
+  document.querySelector('.navigation-item-cart').classList.toggle('navigation-item-cart--open');
+
 })
